d10dafa0922280583290ebc092d110a34fbd8a65
Gilimbek <gilimbek11@gmail.com>
2021-10-12 10:42:25 +0800

    Updates

---
+++ precache-manifest.cd8b827ffa61cc602854a2ced99391f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef7ecd1d7ec8833681ee7a0aa1d45e9",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "cbb20023724c6bb21788",
+    "url": "/burger/static/css/main.4755ebbb.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/burger/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb20023724c6bb21788",
+    "url": "/burger/static/js/main.ddc329ba.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
